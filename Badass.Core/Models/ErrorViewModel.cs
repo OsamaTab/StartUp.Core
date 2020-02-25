b85131417ef87710b5e385d4b29913eb45297e48
Yusuf YURTSEVER <yusuf.mh@outlook.com>
2020-02-26 01:20:20 +0300

    Add project files.

---
+++ Badass.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Badass.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
