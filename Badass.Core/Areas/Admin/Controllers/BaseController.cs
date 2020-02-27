e8577ec4fdee5633d427c7a19736103a1748d8c2
OsamaTap <darkiisouls@outlook.com>
2020-02-27 17:49:48 +0200

    adding authorized for admin

--- Badass.Core/Areas/Admin/Controllers/BaseController.cs
+++ Badass.Core/Areas/Admin/Controllers/BaseController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Badass.Core.Areas.Admin
 {
     [Area("Admin")]
-    //[Authorize]
+    [Authorize]
     public class BaseController : Controller
     {
     }
