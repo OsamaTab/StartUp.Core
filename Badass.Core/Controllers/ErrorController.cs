422628adb16b8831c89d11c22844413b993a27b6
OsamaTap <darkiisouls@outlook.com>
2020-03-15 16:28:10 +0200

    error page inplmented

---
+++ Badass.Core/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Badass.Core.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/Error/{Stats}")]
+        public IActionResult Index(int Stats)
+        {
+            switch (Stats)
+            {
+                case 404:
+                    ViewBag.MessageError = "Sorre Page Not Fount 404";
+                    break;
+                default:
+                    break;
+            }
+            return View("Error");
+        }
+    }
+}
--- Badass.Core/Startup.cs
+++ Badass.Core/Startup.cs
@@ -46,14 +46,15 @@ namespace Badass.Core
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+                //app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage();
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+                //app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
